75ab68515e1c713ca1145f37fb4c3a8013476d48
MmD <mmdj27634@gmail.com>
2023-09-12 14:00:03 -0700

    fix : main page props type error

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,11 +1,8 @@
-import AnimatedLanguage from "@/components/animated-language";
-import Icon from "@/components/icon";
 import Quote from "@/components/quote";
 import About from "@/components/sections/about";
 import Intro from "@/components/sections/intro";
-import { PropsWithChildren } from "react";
 
-export default function Home({}: PropsWithChildren) {
+export default function Home() {
   return (
     <div className="realtive flex min-h-[calc(100vh_-_58px)] w-full">
       <main className="mx-auto flex w-full max-w-[1300px] flex-col items-start justify-between px-6 md:px-24">
