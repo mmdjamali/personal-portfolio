ae6be107f93e19cd9719ed8b618b2ad82c558a84
MmD <mmdj27634@gmail.com>
2023-09-19 07:16:20 -0700

    fix : zoom in mobile

--- components/zoom.tsx
+++ components/zoom.tsx
@@ -5,7 +5,11 @@ import { useEffect } from "react";
 const Zoom = () => {
   useEffect(() => {
     function setZoom() {
+      // stop typescript from yelling :)
       if (!("zoom" in document.body.style)) return;
+
+      // don't zoom in when device in mobile or tablet.
+      if (!/Mobi/i.test(navigator.userAgent)) return;
 
       if (
         window.matchMedia("(min-height: 720px) and (max-height: 900px)").matches
