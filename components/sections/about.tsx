fdf94122d66c5c859c50a6eb0dabe76fccf056f0
MmD <mmdj27634@gmail.com>
2023-11-05 16:54:02 +0330

    change animation of often used technologies

--- components/sections/about.tsx
+++ components/sections/about.tsx
@@ -1,19 +1,19 @@
 "use client";
 
-import React from "react";
-import Icon from "../icon";
+import React, { useRef } from "react";
 
-import { motion } from "framer-motion";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "../ui/tabs";
-import Tech from "../tech";
-import { IconKeyType } from "@/types";
-import Language from "../language";
-import Scrollable from "../scrollable";
+import { motion, useInView } from "framer-motion";
 import NumberUp from "../animation/number-up";
 import { cn } from "@/lib/utils";
 import Image from "next/image";
 
 function About() {
+  const ref = useRef<HTMLDivElement | null>(null);
+  const inView = useInView(ref, {
+    once: true,
+    margin: "0px 0px -25% 0px",
+  });
+
   return (
     <div className="relative grid w-full items-start justify-stretch py-32 sm:px-12">
       <section data-name="about" className="flex w-full flex-col pb-10 sm:px-8">
@@ -35,7 +35,7 @@ function About() {
             }}
             viewport={{
               once: true,
-              margin: "0px 0px -40% 0px",
+              margin: "0px 0px -25% 0px",
             }}
           >
             <span className="inline h-1 w-14 bg-gradient-to-r from-transparent to-foreground" />
@@ -58,7 +58,7 @@ function About() {
             }}
             viewport={{
               once: true,
-              margin: "0px 0px -40% 0px",
+              margin: "0px 0px -25% 0px",
             }}
             className="absolute inset-y-0 left-[calc(100%_+_16px)] top-0 my-auto hidden items-center text-foreground lg:inline-flex"
           >
@@ -71,14 +71,16 @@ function About() {
               duration: 0.5,
             }}
             initial={{
-              opacity: 0.5,
+              y: 60,
+              opacity: 0,
             }}
             whileInView={{
+              y: 0,
               opacity: 1,
             }}
             viewport={{
               once: true,
-              margin: "0px 0px -50% 0px",
+              margin: "0px 0px -25% 0px",
             }}
             className="group relative w-full text-center text-4xl font-bold leading-[1.5] sm:text-6xl"
           >
@@ -106,7 +108,7 @@ function About() {
           }}
           viewport={{
             once: true,
-            margin: "0px 0px -40% 0px",
+            margin: "0px 0px -25% 0px",
           }}
           className=" text-[16px] leading-[1.5] text-foreground/75 lg:px-16"
         >
@@ -114,7 +116,7 @@ function About() {
             "Welcome to my digital realm! I'm Mohammad Jamali, a passionate developer based in "
           }
           <span className="bg-teal-500 px-2 text-white">{"Tabriz, Iran"}</span>
-          {", but fell free to call me MmD(Mamad) if you perfer to."}
+          {", but feel free to call me MmD (Mamad) if you prefer."}
         </motion.p>
 
         <motion.p
@@ -131,7 +133,7 @@ function About() {
           }}
           viewport={{
             once: true,
-            margin: "0px 0px -40% 0px",
+            margin: "0px 0px -25% 0px",
           }}
           className="mt-6 text-[16px] leading-[1.5] text-foreground/75 lg:px-16"
         >
@@ -157,11 +159,11 @@ function About() {
           }}
           viewport={{
             once: true,
-            margin: "0px 0px -40% 0px",
+            margin: "0px 0px -25% 0px",
           }}
           className="mt-6 text-[16px] leading-[1.5] text-foreground/75 lg:px-16"
         >
-          {"Just as wizards use their spells to conjure captivating magic,"}
+          {"Just as wizards use their spells to conjure captivating magic, "}
           <span className="bg-violet-500 px-2 text-white">
             {
               "I harness programming languages and cutting-edge technologies to bring your ideas to life."
@@ -183,84 +185,95 @@ function About() {
           }}
           viewport={{
             once: true,
-            margin: "0px 0px -40% 0px",
+            margin: "0px 0px -25% 0px",
           }}
           className="mt-6 text-[16px] leading-[1.5] text-foreground/75 lg:px-16"
         >
           {"Here are some technologies that I use often :"}
         </motion.p>
 
-        <motion.div
-          transition={{
-            duration: 0.5,
-          }}
-          initial={{
-            y: 60,
-            opacity: 0,
-          }}
-          whileInView={{
-            y: 0,
-            opacity: 1,
-          }}
-          viewport={{
-            once: true,
-            margin: "0px 0px -50% 0px",
-          }}
+        <div
+          ref={ref}
           className="relative mt-8 grid w-full grid-cols-[repeat(auto-fit,minmax(280px,1fr))] gap-6 lg:px-16"
         >
-          {often_used_tech.map(({ name, bg, border, color, date, icon }) => {
-            return (
-              <div className="relative" key={name}>
-                <div
-                  className={cn("absolute z-[1] h-full w-full opacity-75", bg)}
-                />
-
-                <div
-                  className={cn(
-                    "relative z-[2] flex h-16 w-full -translate-x-1.5 -translate-y-1.5 items-center overflow-hidden border-[6px] bg-background/95 px-4 py-2",
-                    border,
-                    color,
-                  )}
+          {often_used_tech.map(
+            ({ name, bg, border, color, date, icon }, idx) => {
+              return (
+                <motion.div
+                  transition={{
+                    duration: 0.5,
+                    delay: idx * 0.3 + 0.5,
+                  }}
+                  initial={{
+                    y: 60,
+                    opacity: 0,
+                  }}
+                  animate={
+                    inView
+                      ? {
+                          y: 0,
+                          opacity: 1,
+                        }
+                      : {}
+                  }
+                  className="relative"
+                  key={name}
                 >
-                  <div className="flex h-fit w-full items-center gap-4 text-background">
-                    <Image
-                      src={icon}
-                      height={35}
-                      width={35}
-                      unoptimized
-                      alt={name}
-                      className="object-contain"
-                    />
+                  <div
+                    className={cn(
+                      "absolute z-[1] h-full w-full opacity-75",
+                      bg,
+                    )}
+                  />
 
-                    <div
-                      className={cn(
-                        "flex w-full items-center justify-between",
-                        color,
-                      )}
-                    >
-                      <span className="overflow-hidden text-ellipsis text-[16px] font-medium">
-                        {name}
-                      </span>
-                      <span className="flex items-center">
-                        +
-                        <NumberUp
-                          minLen={2}
-                          key={"test"}
-                          duration={1000}
-                          number={Math.ceil(
-                            (new Date().getTime() - new Date(date).getTime()) /
-                              2629746000,
-                          )}
-                        />
-                        <span>&nbsp; months</span>
-                      </span>
+                  <div
+                    className={cn(
+                      "relative z-[2] flex h-16 w-full -translate-x-1.5 -translate-y-1.5 items-center overflow-hidden border-[6px] bg-background/95 px-4 py-2",
+                      border,
+                      color,
+                    )}
+                  >
+                    <div className="flex h-fit w-full items-center gap-4 text-background">
+                      <Image
+                        src={icon}
+                        height={35}
+                        width={35}
+                        unoptimized
+                        alt={name}
+                        className="object-contain"
+                      />
+
+                      <div
+                        className={cn(
+                          "flex w-full items-center justify-between",
+                          color,
+                        )}
+                      >
+                        <span className="overflow-hidden text-ellipsis text-[16px] font-medium">
+                          {name}
+                        </span>
+                        <span className="flex items-center">
+                          +
+                          <NumberUp
+                            minLen={2}
+                            key={"test"}
+                            duration={1000}
+                            number={Math.ceil(
+                              (new Date().getTime() -
+                                new Date(date).getTime()) /
+                                2629746000,
+                            )}
+                          />
+                          <span>&nbsp; months</span>
+                        </span>
+                      </div>
                     </div>
                   </div>
-                </div>
-              </div>
-            );
-          })}
-        </motion.div>
+                </motion.div>
+              );
+            },
+          )}
+        </div>
       </section>
     </div>
   );
