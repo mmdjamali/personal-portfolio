b6457361d931ececad5730150605c51c23497350
MmD <mmdj27634@gmail.com>
2023-12-07 01:07:14 +0330

    add : tanstack query provider

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -4,8 +4,16 @@ import { PropsWithChildren } from "react";
 
 import { ThemeProvider } from "next-themes";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
 const Providers = ({ children }: PropsWithChildren) => {
-  return <ThemeProvider defaultTheme="light">{children}</ThemeProvider>;
+  const client = new QueryClient();
+
+  return (
+    <QueryClientProvider client={client}>
+      <ThemeProvider defaultTheme="light">{children}</ThemeProvider>
+    </QueryClientProvider>
+  );
 };
 
 export default Providers;
