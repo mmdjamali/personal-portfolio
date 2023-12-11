f16693f7037a768eefe5bcf76aca59b78635ddd6
MmD <mmdj27634@gmail.com>
2023-12-11 20:49:27 +0330

    make login page more minimalist

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -8,14 +8,7 @@ export const metadata: Metadata = {
 
 const Page = () => {
   return (
-    <main className="grid h-full w-full grid-cols-1 place-items-center md:grid-cols-2">
-      <div className="hidden h-full w-full items-center justify-center bg-black md:flex">
-        <Icon
-          name="Crown"
-          className="m-12 aspect-square w-full max-w-xs text-white"
-        />
-      </div>
-
+    <main className="grid h-full w-full grid-cols-1 place-items-center">
       <AdminLogin />
     </main>
   );
