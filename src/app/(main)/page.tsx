dd3aecf373c2282930659d031b7016ca4b5b62a1
MmD <mmdj27634@gmail.com>
2023-12-12 00:16:44 +0330

    add : logic to fetch technologies and pass to about

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -3,8 +3,66 @@ import About from "@/components/sections/about";
 import ContactMe from "@/components/sections/contact-me";
 import Intro from "@/components/sections/intro";
 import Projects from "@/components/sections/projects";
+import { env } from "@/env";
+import { ApiResponse } from "@/types/api";
+import { TechnologyEntity } from "@/types/entity";
 
-export default function Home() {
+const fetchOftenUsedTechnologies = async () => {
+  let error;
+  let data;
+  try {
+    const res: ApiResponse<{ rows: TechnologyEntity[] }> = await fetch(
+      env.NEXT_PUBLIC_BACKEND_URL + "/api/used-often",
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      },
+    ).then((res) => res?.json());
+
+    if (!res?.success) throw "Request was not successfull";
+
+    data = res.data.rows;
+  } catch (err) {
+    error = err;
+  }
+
+  return {
+    data,
+    error,
+  };
+};
+
+const fetchShowcaseProjects = async () => {
+  let error;
+  let data;
+  try {
+    const res: ApiResponse<{ rows: TechnologyEntity }> = await fetch(
+      env.NEXT_PUBLIC_BACKEND_URL + "/api/technology/often-used",
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      },
+    ).then((res) => res?.json());
+
+    if (!res?.success) throw "Request was not successfull";
+
+    data = res.data.rows;
+  } catch (err) {
+    error = err;
+  }
+
+  return {
+    data,
+    error,
+  };
+};
+
+export default async function Home() {
+  const { data: technologies, error: technologiesError } =
+    await fetchOftenUsedTechnologies();
+
   return (
     <div className="realtive flex min-h-[calc(100svh_-_58px)] w-full">
       <main className="relative mx-auto flex w-full max-w-[1300px] flex-col items-start justify-between px-6 md:px-24">
@@ -15,7 +73,7 @@ export default function Home() {
           quote="The only person who is educated is the one who learned how to learn and change"
         />
 
-        <About />
+        <About technologies={technologies ?? []} />
 
         <Quote
           by="Carl rogers"
