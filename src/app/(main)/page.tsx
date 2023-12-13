2d64ec62ea04525115b79b9f3f7b1c6b78bf6bf1
MmD <mmdj27634@gmail.com>
2023-12-13 20:52:56 +0330

    add : logic for fecthing projects and passing them to component

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -5,7 +5,7 @@ import Intro from "@/components/sections/intro";
 import Projects from "@/components/sections/projects";
 import { env } from "@/env";
 import { ApiResponse } from "@/types/api";
-import { TechnologyEntity } from "@/types/entity";
+import { ProjectEntity, TechnologyEntity } from "@/types/entity";
 
 const fetchOftenUsedTechnologies = async () => {
   let error;
@@ -38,18 +38,19 @@ const fetchShowcaseProjects = async () => {
   let error;
   let data;
   try {
-    const res: ApiResponse<{ rows: TechnologyEntity }> = await fetch(
-      env.NEXT_PUBLIC_BACKEND_URL + "/api/technology/often-used",
+    const res: ApiResponse<{ projects: ProjectEntity[] }> = await fetch(
+      env.NEXT_PUBLIC_BACKEND_URL + "/api/project/showcase",
       {
         headers: {
           "Content-Type": "application/json",
         },
+        cache: "no-cache",
       },
     ).then((res) => res?.json());
 
     if (!res?.success) throw "Request was not successfull";
 
-    data = res.data.rows;
+    data = res.data.projects;
   } catch (err) {
     error = err;
   }
@@ -63,6 +64,9 @@ const fetchShowcaseProjects = async () => {
 export default async function Home() {
   const { data: technologies, error: technologiesError } =
     await fetchOftenUsedTechnologies();
+
+  const { data: projects, error: projectsError } =
+    await fetchShowcaseProjects();
 
   return (
     <div className="realtive flex min-h-[calc(100svh_-_58px)] w-full">
@@ -81,7 +85,7 @@ export default async function Home() {
           quote="The only person who is educated is the one who learned how to learn and change"
         />
 
-        <Projects />
+        <Projects projects={projects ?? []} />
 
         <Quote
           by="Carl rogers"
