a666b6fe06162f318f830f62ed88f63251d96f7a
MmD <mmdj27634@gmail.com>
2023-12-14 20:28:06 +0330

    add : pulse animation to loading icon

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -3,10 +3,12 @@ import Icon from "@/components/icon";
 const Loading = () => {
   return (
     <div className="grid h-full w-full place-items-center bg-background">
-      <Icon
-        name="Crown"
-        className="h-[70px] text-foreground duration-500 animate-in slide-in-from-bottom-24"
-      />
+      <div className="animate-pulse ease-emil">
+        <Icon
+          name="Crown"
+          className="h-[70px] text-foreground duration-500 animate-in slide-in-from-bottom-24"
+        />
+      </div>
     </div>
   );
 };
