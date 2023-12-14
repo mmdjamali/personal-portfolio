e0542c6f8cd31c47e13d63ee26647f5a8b8de38a
MmD <mmdj27634@gmail.com>
2023-12-14 20:11:33 +0330

    get socials from props

--- src/components/social-medias.tsx
+++ src/components/social-medias.tsx
@@ -3,20 +3,24 @@
 import { IconKeyType } from "@/types";
 import Icon from "./icon";
 import Link from "next/link";
-import { socials } from "@/config/socials";
+import { SocialsEntity } from "@/types/entity";
 
-const SocialMedias = () => {
+type Props = {
+  socials: SocialsEntity[];
+};
+
+const SocialMedias = ({ socials }: Props) => {
   return (
     <div className="pointer-events-none fixed inset-0 z-[50] mx-auto hidden w-full max-w-[1300px] justify-end px-6 md:flex md:px-24">
       <div className="z-[50] my-auto hidden h-fit flex-col md:flex">
-        {socials.map(({ icon, name, url }, idx) => {
+        {socials?.map(({ platform, url }, idx) => {
           return (
             <Link
-              href={url}
+              href={url ?? ""}
               className="pointer-events-auto z-[50] p-2 text-foreground/75 transition-colors hover:text-foreground"
               key={idx}
             >
-              <Icon className="text-[21px]" name={icon} />
+              <Icon className="text-[21px]" name={platform} />
             </Link>
           );
         })}
